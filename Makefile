05135f0229ef276cc39dc8cfdefd5c8a335ac079
Lidia Mokevnina <lidia.mokevnina@dualbootpartners.com>
2025-12-02 20:48:59 +0100

    added size calculation

--- Makefile
+++ Makefile
@@ -2,4 +2,10 @@ build:
 	go build -o bin/hexlet-path-size ./cmd/hexlet-path-size
 
 run:
-	./bin/hexlet-path-size
+	./bin/hexlet-path-size $(path)
+
+lint:
+	golangci-lint run
+
+dev:
+	go run ./cmd/hexlet-path-size $(path)
--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/urfave/cli/v3"
 )
@@ -13,13 +15,61 @@ func main() {
 	cmd := &cli.Command{
 		Name:  "hexlet-path-size",
 		Usage: "print size of a file or directory",
-		Action: func(context.Context, *cli.Command) error {
-			fmt.Println("Hello from Hexlet")
+		Action: func(_ context.Context, cmd *cli.Command) error {
+			path := cmd.Args().Get(0)
+			size, err := GetPathSize(path, false, true, false)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			fmt.Println(size)
 			return nil
 		},
 	}
 
 	if err := cmd.Run(context.Background(), os.Args); err != nil {
-		log.Fatal(err)
+		log.Fatal()
 	}
+}
+
+func GetPathSize(path string, recursive, human, all bool) (string, error) {
+	size, err := GetSize(path)
+	if err != nil {
+		return "", errors.New(err.Error())
+	}
+
+	formattedOutput := fmt.Sprintf("%s\t%s", strconv.Itoa(size), path)
+
+	return formattedOutput, nil
+}
+
+func GetSize(path string) (int, error) {
+	fileInfo, err := os.Lstat(path)
+	if err != nil {
+		return 0, errors.New("the directory or file does not exist")
+	}
+	if fileInfo.IsDir() {
+		size, err := GetDirSize(path)
+		if err != nil {
+			return 0, errors.New(err.Error())
+		}
+		return size, nil
+	} else {
+		return int(fileInfo.Size()), nil
+	}
+}
+
+func GetDirSize(path string) (int, error) {
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return 0, errors.New("couldn't read the directory")
+	}
+	size := 0
+	for _, file := range files {
+		info, err := file.Info()
+		if err != nil {
+			return 0, errors.New("couldn't get file size")
+		}
+		size += int(info.Size())
+	}
+	return size, nil
 }
